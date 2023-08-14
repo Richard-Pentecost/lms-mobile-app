c2e4d47a5e38dc36fd06e7367e4455ae86a0c522
Richard Pentecost <richard.pentecost@madetech.com>
2023-08-14 15:51:49 +0100

    add test environment variables

--- __tests__/screens/LoginScreen.test.js
+++ __tests__/screens/LoginScreen.test.js
@@ -1,9 +1,26 @@
-import { screen } from '@testing-library/react-native';
+import { act, fireEvent, screen } from '@testing-library/react-native';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { Alert } from 'react-native';
+import { API_URL } from 'react-native-dotenv';
 import LoginScreen from '../../screens/LoginScreen';
 import { renderWithProviders } from '../../utils/utilsForTests';
 
+export const handlers = [
+  rest.post(`${API_URL}/login`, (_req, res, ctx) => {
+    return res(ctx.status(401), ctx.json({ error: 'Incorrect email ' }));
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('Login Screen', () => {
+  beforeEach(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
   it('should render', () => {
     renderWithProviders(<LoginScreen />);
 
@@ -28,5 +45,26 @@ describe('Login Screen', () => {
       'There is an error',
       'The email or password you entered was incorrect. Please try again.'
     );
+  });
+
+  describe('Integration test for login failure when the password or email is not correct', () => {
+    it('shows an alert when the email or password is incorrect', async () => {
+      Alert.alert = jest.fn();
+
+      renderWithProviders(<LoginScreen />);
+
+      await act(async () => {
+        await fireEvent.changeText(
+          screen.getByTestId('email'),
+          'invalid@email.com'
+        );
+        await fireEvent.changeText(screen.getByTestId('password'), 'password');
+        fireEvent.press(screen.getByTestId('submit'));
+      });
+      expect(Alert.alert).toHaveBeenCalledWith(
+        'Incorrect Login Details',
+        'The email or password you entered was incorrect. Please try again.'
+      );
+    });
   });
 });
--- testSetup.js
+++ testSetup.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
+
+dotenv.config({ path: './.env.test' });
